6350fa6992eff871da2961d0cf93136425081a5a
Jude Solis <jude.solis@service-now.com>
2015-10-26 11:19:48 -0700

    first pass.  should be working enough to give the cafe lunch.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var cheerio = require('cheerio');
+
 module.exports = (function () {
     function handler(robot) {
         robot.respond(/what's for lunch/i, sayLunch);
@@ -9,13 +11,35 @@ module.exports = (function () {
     }
 
     function fetchCafeMenu() {
-        return "CAFE MENU:\n\n/quote BREAKFAST SPECIAL\n" +
-        "Sauteed Kale, Feta, Roasted Red Pepper Omelet with a side of Seasonal Fruit\n" +
-        "$4.75\n\n" +
+        var thisMonday = getThisMonday();
+        var dayOfWorkWeek = getDayOfWorkWeek();
+        var menuPath = "9PEU5T~" + thisMonday + "/$file/day" +
+            dayOfWorkWeek + ".htm";
+        var todaysMenuUrl = "http://dining.guckenheimer.com/clients/servicenowsd/fss/fss.nsf/weeklyMenuLaunch/" +
+            menuPath;
 
-        "SOUPS\n" +
-        "Smoked Ham and Spit Pea Soup\n" +
-        "$2.25~$3.25";
+        robot.http(todaysMenuUrl).get(function (err, res, body) {
+            var menuDom = cheerio.load(body);
+            return menuDom(".center_text").text();
+        });
+    }
+
+    function getThisMonday() {
+        var today = new Date();
+        var month = leadingZero(today.getMonth + 1);
+        var dayOfMonth = today.getDate();
+        var daysSinceMonday = today.getDay() - 1;
+        var mondayThisWeek = leadingZero(dayOfMonth - daysSinceMonday);
+        var year = today.getFullYear();
+        return month + "-" + mondayThisWeek + "-" + year;
+    }
+
+    function leadingZero(base) {
+        return ("000" + base).substr(-2);
+    }
+
+    function getDayOfWorkWeek() {
+        return new Date().getDay();
     }
 
     return handler;
